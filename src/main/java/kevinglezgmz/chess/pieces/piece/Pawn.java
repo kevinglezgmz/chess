b0337e76ba6316abb56f6f8b03107137975e4235
kevinglezgmz <44679178+kevinglezgmz@users.noreply.github.com>
2020-07-07 23:42:10 -0500

    chore: setup maven project to be able to test the code in the app (#4)
    * refactored the code for the chess pieces and added some useful functions inside the abstract ChessPiece class
    
    * implemented functionality inside the GameEngine class
    
    * chore: setup a maven project to be able to test the code in the app

--- src/main/java/kevinglezgmz/chess/pieces/piece/Pawn.java
+++ src/main/java/kevinglezgmz/chess/pieces/piece/Pawn.java
@@ -1,6 +1,6 @@
-package Chess.ChessPieces.Pieces;
+package kevinglezgmz.chess.pieces.piece;
 
-import Chess.ChessPieces.ChessPiece;
+import kevinglezgmz.chess.pieces.ChessPiece;
 
 public class Pawn extends ChessPiece {
   public static char symbol = 'P';
